e4ad8eeda19e6c9782cca7bf966218ee2668da55
ViniInvert <vinimakegames@gmail.com>
2022-05-18 23:36:32 -0300

    Changes to Input Manager
    Added a function to determine the cursor position and hovered object.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -15,15 +15,31 @@ public class InputManager : MonoBehaviour
 
     void Update()
     {
+        CursorPositionUpdate();
         MouseInput();
     }
-    public Transform testCube;
-    void MouseInput()
+
+    Vector3 _cursorPosition;
+    Transform _hoveredObject;
+    void CursorPositionUpdate()
     {
         _ray = _mainCam.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(_ray, out _hit))
+        if (Physics.Raycast(_ray, out _hit))
         {
-            testCube.position = _hit.point;
+            _cursorPosition = _hit.point;
+            _hoveredObject = _hit.transform;
+        }
+    }
+
+    int _selectedTower = -1;
+    void MouseInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(_selectedTower > -1 && _hoveredObject.tag == "TowerSlot" && _hoveredObject.childCount == 1)
+            {
+                //Instantiate tower
+            }
         }
     }
 }
